18e7b5158cc267c8a6e5d6d7cae231fc6057e4ab
Patrick Browning <patrick@browninglogic.com>
2019-02-13 20:41:19 -0500

    Configure CORS, Swagger UI, and url validation

--- index.js
+++ index.js
@@ -2,22 +2,70 @@ const express = require("express");
 const internetRadio = require('node-internet-radio');
 const app = express();
 const port = 3000;
+const cors = require('cors');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const { query, validationResult } = require('express-validator/check');
 
 app.listen(port, () => {
  console.log(`Server running on port ${port}`);
 });
 
+// Configure CORS
+app.use(cors());
+
+// Configure Swagger UI
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerJSDoc({
+    swaggerDefinition: swaggerDefinition = {
+        info: {
+            title: 'Internet Radio Metadata API',
+            version: '0.0.1',
+            description: "API that retrieves the Now Playing metadata of radio streams via the node-internet-radio module"
+        },
+        host: 'localhost:3000',
+        basePath: '/',
+    },
+    apis: ['index.js'],
+})));
+
 /**
- * Listen on /metadata/url for a metadata request.  This is
- * an API wrapper around the getStationInfo function provided
- * by https://www.npmjs.com/package/node-internet-radio.
- * 'url' should be the url-decoded radio station URL.
- * 'method' is an optional query param which specifies the stream
- * source method to query, as specified in the node-internet-radio
- * package readme.
+ * @swagger
+ * /now-playing:
+ *  get:
+ *      summary: Gets the 'Now Playing' metadata for the specified radio stream URL
+ *      description: This is an API wrapper around the getStationInfo function 
+ *          provided by https://www.npmjs.com/package/node-internet-radio.
+ *      produces:
+ *      - "application/json"
+ *      parameters:
+ *      - name: url
+ *        in: query
+ *        description: Encoded URL to fetch metadata for
+ *        required: true
+ *        type: string
+ *        example: http%3A%2F%2F188.165.212.92%3A8000%2Fheavy128mp3
+ *      - name: method
+ *        in: query
+ *        description: The stream source method to query, as explained
+ *          in the node-internet-radio readme.  This is optional, but
+ *          strongly recommended.
+ *        required: false
+ *        type: string
+ *        example: STREAM
+ *      responses:
+ *        200:
+ *          description: Metadata was successfully retrieved
+ *        400:
+ *          description: Validation error - url was not provided or is not a valid url
+ *        500:
+ *          description: An error was reported by getStationInfo
  */
-app.get("/metadata/:url", (req, res) => {
-    internetRadio.getStationInfo(req.params.url, function(error, station) {
+app.get("/now-playing", query('url').isURL(), (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    internetRadio.getStationInfo(req.query.url, function(error, station) {
         // If there was an error, then report it back with a 500
         if(error) {
             res.status(500).send(error.message);
