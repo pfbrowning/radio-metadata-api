48624c9228be105c1673d01c15a556398f095edc
unknown <patrick@browninglogic.com>
2019-07-14 17:51:18 -0400

    Move now-playing controller to its own file

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const routes = require('express').Router();
-const internetRadio = require('node-internet-radio')
-const { query, validationResult } = require('express-validator/check')
+const { query } = require('express-validator/check')
+const nowPlaying = require('./controllers/now-playing');
 
 /**
  * @swagger
@@ -34,23 +34,6 @@ const { query, validationResult } = require('express-validator/check')
  *        500:
  *          description: An error was reported by getStationInfo
  */
-routes.get('/now-playing', query('url').exists(), (req, res) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() })
-    }
-    internetRadio.getStationInfo(decodeURIComponent(req.query.url), function (error, station) {
-      // If there was an error, then report it back with a 500
-      if (error) {
-        res.status(500).send(error.message)
-      // If the call was successful, then return the data with a 200
-      } else {
-        res.status(200).send(station)
-      }
-      /* Pass in query method in case the user provided a value.
-      If nothing was provided, then getStationInfo will check all
-      supported protocols, which is easy but inefficient. */
-    }, req.query.method)
-  })
+routes.get('/now-playing', query('url').exists(), nowPlaying.apiGET);
   
 module.exports = routes;
