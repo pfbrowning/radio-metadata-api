a1f2595cc78bf932f44bf12d13213cb173e3bbbb
unknown <patrick@browninglogic.com>
2019-07-20 13:54:44 -0400

    Pull version number from package.json for swagger

--- middlewares/swagger.js
+++ middlewares/swagger.js
@@ -1,11 +1,12 @@
 const swaggerJSDoc = require('swagger-jsdoc')
 const swaggerUi = require('swagger-ui-express')
+const packageVersion = require('../package.json').version;
 
 module.exports = swaggerUi.setup(swaggerJSDoc({
   swaggerDefinition: {
     info: {
       title: 'Internet Radio Metadata API',
-      version: '0.0.1',
+      version: packageVersion,
       description: 'API that retrieves the Now Playing metadata of radio streams via the node-internet-radio module'
     },
     host: 'localhost:3000',
