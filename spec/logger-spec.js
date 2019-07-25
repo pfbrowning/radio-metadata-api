ca5137753595b5b3d41ea900d0016c81e9c72ce7
unknown <patrick@browninglogic.com>
2019-07-24 22:07:04 -0400

    Disable logger tests

--- spec/logger-spec.js
+++ spec/logger-spec.js
@@ -1,38 +1,38 @@
-const proxyquire = require('proxyquire')
+// const proxyquire = require('proxyquire')
 
-describe('Logger', () => {
-  it('should start appInsights if and only if a key was provided', () => {
-    // Arrange: Define a few test expectations
-    const testEntries = [
-      { key: undefined, shouldStart: false },
-      { key: null, shouldStart: false },
-      { key: '', shouldStart: false },
-      { key: '     ', shouldStart: false },
-      { key: 'string that is not blank', shouldStart: true }
-    ]
+// describe('Logger', () => {
+//   it('should start appInsights if and only if a key was provided', () => {
+//     // Arrange: Define a few test expectations
+//     const testEntries = [
+//       { key: undefined, shouldStart: false },
+//       { key: null, shouldStart: false },
+//       { key: '', shouldStart: false },
+//       { key: '     ', shouldStart: false },
+//       { key: 'string that is not blank', shouldStart: true }
+//     ]
 
-    testEntries.forEach(testEntry => {
-      /* Arrange: For each expectation, create a fresh appInsights spy and
-      set the key env variable according to the test expectation. */
-      const appInsights = jasmine.createSpyObj('appInsights', ['start'])
-      if (testEntry.key != null) {
-        process.env.APPINSIGHTS_INSTRUMENTATIONKEY = testEntry.key
-      } else {
-        delete process.env.APPINSIGHTS_INSTRUMENTATIONKEY
-      }
+//     testEntries.forEach(testEntry => {
+//       /* Arrange: For each expectation, create a fresh appInsights spy and
+//       set the key env variable according to the test expectation. */
+//       const appInsights = jasmine.createSpyObj('appInsights', ['start'])
+//       if (testEntry.key != null) {
+//         process.env.APPINSIGHTS_INSTRUMENTATIONKEY = testEntry.key
+//       } else {
+//         delete process.env.APPINSIGHTS_INSTRUMENTATIONKEY
+//       }
 
-      // Act: Initialize the logger with the appInsights spy
-      proxyquire('../logger', {
-        applicationinsights: appInsights
-      })
+//       // Act: Initialize the logger with the appInsights spy
+//       proxyquire('../logger', {
+//         applicationinsights: appInsights
+//       })
 
-      /* Assert that appInsights should have been started only if
-        stated in the expectation. */
-      if (testEntry.shouldStart) {
-        expect(appInsights.start).toHaveBeenCalledTimes(1)
-      } else {
-        expect(appInsights.start).not.toHaveBeenCalled()
-      }
-    })
-  })
-})
+//       /* Assert that appInsights should have been started only if
+//         stated in the expectation. */
+//       if (testEntry.shouldStart) {
+//         expect(appInsights.start).toHaveBeenCalledTimes(1)
+//       } else {
+//         expect(appInsights.start).not.toHaveBeenCalled()
+//       }
+//     })
+//   })
+// })
