a20d935f364a92132c1706cf3ab9fbd5e092a99b
unknown <patrick@browninglogic.com>
2019-07-25 00:52:09 -0400

    Configure appinsights winston transport

--- logger.js
+++ logger.js
@@ -1,20 +1,26 @@
 const appInsights = require('applicationinsights')
 const winston = require('winston')
 const DailyRotateFile = require('winston-daily-rotate-file')
+const { AzureApplicationInsightsLogger } = require('winston-azure-application-insights')
 const isBlank = require('is-blank')
 const appInsightsKey = process.env.APPINSIGHTS_INSTRUMENTATIONKEY
+
+const transports = [
+  new winston.transports.Console({ handleExceptions: true }),
+  new DailyRotateFile({ filename: 'logs/log-%DATE%.log', handleExceptions: true })
+]
 
 // Use Azure App Insights if and only if an instrumentation key is present
 if (!isBlank(appInsightsKey)) {
   appInsights.setup(appInsightsKey).start()
+  transports.push(new AzureApplicationInsightsLogger({
+    insights: appInsights
+  }))
 }
 
 // Log to the console and a daily rolling log
 const logger = winston.createLogger({
-  transports: [
-    new winston.transports.Console({ handleExceptions: true }),
-    new DailyRotateFile({ filename: 'logs/log-%DATE%.log', handleExceptions: true })
-  ]
+  transports: transports
 })
 
 logger.stream = {
