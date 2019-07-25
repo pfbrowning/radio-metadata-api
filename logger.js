230831c42744d9e876e23d0235775b69d7063c84
unknown <patrick@browninglogic.com>
2019-07-24 21:54:05 -0400

    Temporarily disable appinsights logging

--- logger.js
+++ logger.js
@@ -2,7 +2,7 @@ const appInsights = require('applicationinsights')
 const isBlank = require('is-blank')
 const appInsightsKey = process.env.APPINSIGHTS_INSTRUMENTATIONKEY
 
-// Use Azure App Insights if and only if an instrumentation key is present
-if (!isBlank(appInsightsKey)) {
-  appInsights.start()
-}
+// // Use Azure App Insights if and only if an instrumentation key is present
+// if (!isBlank(appInsightsKey)) {
+//   appInsights.start()
+// }
